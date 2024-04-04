bb9875a69aa063a72e81be816db077bc52bfdb6e
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-04-04 18:31:52 -0300

    foto products y users

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,7 +27,6 @@ function Products() {
             <h2>{each.title}</h2>
             <img src={each.photo} alt={each.title} />
             <h4>${each.price}</h4>
-            <h4>Categoría: {each.category}</h4>
             <h5>Stock: {each.stock}</h5>
           </div>
         ))}
