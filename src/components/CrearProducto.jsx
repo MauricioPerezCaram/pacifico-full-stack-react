64a9078f6b2e13818d7b8990d9496cfeff856bfa
Mauricio Perez Caram <perezcarammauricio@gmail.com>
2024-04-07 16:50:20 -0300

    maquetado crearproducto

--- src/components/CrearProducto.jsx
+++ src/components/CrearProducto.jsx
@@ -1,7 +1,38 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 function CrearProducto() {
   return (
     <div>
-      <h1>Crear producto</h1>
+      <Form>
+        <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
+          <Form.Label>Producto</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
+          <Form.Label>Categoría</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
+          <Form.Label>Precio</Form.Label>
+          <Form.Control type="number" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
+          <Form.Label>Stock</Form.Label>
+          <Form.Control type="number" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
+          <Form.Label>Imagen</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
+          <Form.Label>Descripcion</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Agregar producto
+        </Button>
+      </Form>
     </div>
   );
 }
