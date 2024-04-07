4ff102cead24f93385c36b7c793ea3216f9d28cf
Mauricio Perez Caram <perezcarammauricio@gmail.com>
2024-04-07 16:27:39 -0300

    ruteo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 function BarraNavegacion() {
   return (
@@ -9,14 +10,33 @@ function BarraNavegacion() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#inicio">Inicio</Nav.Link>
-            <Nav.Link href="#productos">Productos</Nav.Link>
-            <Nav.Link href="#creaunproducto">Crea un producto</Nav.Link>
-            <Nav.Link href="#carrito">Carrito</Nav.Link>
-            <Nav.Link href="#registrate">Registrate</Nav.Link>
-            <Nav.Link href="#perfil">Perfil</Nav.Link>
-            <Nav.Link href="#iniciarsesión">Iniciar sesión</Nav.Link>
-            <Nav.Link href="#cerrarsesión">Cerrar sesión</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Inicio
+            </Nav.Link>
+            <Nav.Link as={Link} to="/productos">
+              Productos
+            </Nav.Link>
+            <Nav.Link as={Link} to="/creaunproducto">
+              Crea un producto
+            </Nav.Link>
+            <Nav.Link as={Link} to="/carrito">
+              Carrito
+            </Nav.Link>
+            <Nav.Link as={Link} to="/registrate">
+              Registrate
+            </Nav.Link>
+            <Nav.Link as={Link} to="/usuarios">
+              Usuarios
+            </Nav.Link>
+            <Nav.Link as={Link} to="/perfil">
+              Perfil
+            </Nav.Link>
+            <Nav.Link as={Link} to="/iniciarsesión">
+              Iniciar sesión
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cerrarsesión">
+              Cerrar sesión
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
